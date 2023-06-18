ed85e6ab37c570e073ad043aef4cba8cee76faf2
Sagar Kumar <idal.sagar@gmail.com>
2023-06-18 17:20:35 +0530

    Added initial spring boot commit

---
+++ src/test/java/com/talk2doc/Talk2docApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talk2doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Talk2docApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
